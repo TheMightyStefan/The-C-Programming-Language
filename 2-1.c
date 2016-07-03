ae0f3014cd126b6377ff127a1fb948b29c6e1550
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-07-03 22:28:47 +0300

    Nu merge

--- 2-1.c
+++ 2-1.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+
+long long pow2(int exponent) 
+{
+	 int pow = 1;
+	 while (exponent > 0) {
+		pow *= 2;
+	 }
+	return pow;
+}
 
 int main ()
 {
-	// char
-	printf("CHAR : -%f     -----     %f\n", pow (2.0, (sizeof(char) * 8) - 1), pow (2.0, (sizeof(char) * 8) - 1) - 1);
-	printf("SHORT : -%f     -----     %f\n", pow (2.0, (sizeof(short) * 8) - 1), pow (2.0, (sizeof(short) * 8) - 1) - 1);
-	printf("INT : -%f     -----     %f\n", pow (2.0, (sizeof(int) * 8) - 1), pow (2.0, (sizeof(int) * 8) - 1) - 1);
-        printf("SIGNED LONG : %d     -----     %f\n", 0, pow (2.L, (sizeof(signed long) * 8)));
-        printf("SIGNED LONG LONG : %d     -----     %f\n", 0, pow (2.L, (sizeof(signed long long) * 8)));
-	printf("UNSIGNED LONG : -%f   -----     %f\n", pow (2.L, (sizeof(unsigned long) * 8) - 1), pow (2.L, (sizeof(unsigned long) * 8) - 1) - 1);
-	printf("UNSIGNED LONG LONG : -%f     -----     %f\n", pow (2.L, (sizeof(unsigned long long) * 8) - 1), pow (2.L, (sizeof(unsigned long long) * 8) - 1) - 1);
+	
+	printf("CHAR : -%lld     -----     %lld\n", pow2((sizeof(char) * 8) - 1), pow2((sizeof(char) * 8) - 1) - 1);
+	printf("SHORT : -%lld     -----     %lld\n", pow2((sizeof(short) * 8) - 1), pow2((sizeof(short) * 8) - 1) - 1);
+	printf("INT : -%lld -----     %lld\n", pow2((sizeof(int) * 8) - 1), pow2((sizeof(int) * 8) - 1) - 1);
+        printf("SIGNED LONG : 0     -----     %lld\n", pow2((sizeof(signed long) * 8)));
+        printf("SIGNED LONG LONG : 0     -----     %lld\n", pow2((sizeof(signed long long) * 8)));
+	printf("UNSIGNED LONG : -%lld   -----     %lld\n", pow2((sizeof(unsigned long) * 8) - 1), pow2((sizeof(unsigned long) * 8) - 1) - 1);
+	printf("UNSIGNED LONG LONG : -%lld     -----     %lld\n", pow2((sizeof(unsigned long long) * 8) - 1), pow2((sizeof(unsigned long long) * 8) - 1) - 1);
 	return 0;
 }  
