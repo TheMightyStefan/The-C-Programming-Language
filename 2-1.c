a42d418851545a15bbe0198cb6a37da2571c835f
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-07-05 09:39:25 +0300

    Fixed max_range for unsigned types

--- 2-1.c
+++ 2-1.c
@@ -18,17 +18,11 @@ int main()
 
 	printf("\nCHAR : %lld    ----    %lld\n", min_range, max_range);
 
-	min_range = 0;
-	max_range = 0;
-
 	//short
 	min_range = -pow2((sizeof(short) * 8) - 1);
 	max_range =  pow2((sizeof(short) * 8) - 1) - 1;
 
 	printf("SHORT : %lld    ----    %lld\n", min_range, max_range);
-
-	min_range = 0;
-	max_range = 0;
 
 	//int
 	min_range = -pow2((sizeof(int) * 8) - 1);
@@ -36,17 +30,11 @@ int main()
 
 	printf("INT : %lld    ----    %lld\n", min_range, max_range);
 
-	min_range = 0;
-	max_range = 0;
-
 	//long
 	min_range = -pow2((sizeof(long) * 8) - 1);
 	max_range =  pow2((sizeof(long) * 8) - 1) - 1;
 
 	printf("LONG : %lld    ----    %lld\n", min_range, max_range);
-
-	min_range = 0;
-	max_range = 0;
 
 	//long long
 	min_range = -pow2((sizeof(long long) * 8) - 1);
@@ -54,26 +42,17 @@ int main()
 
 	printf("LONG LONG : %lld    ----    %lld\n", min_range, max_range);
 
-	min_range = 0;
-	max_range = 0;
-
 	//unsigned long
 	min_range = 0;
-	max_range = pow2(sizeof(unsigned long) * 8);
+	max_range = pow2(sizeof(unsigned long) * 8) - 1;
 
 	printf("UNSIGNED LONG : %llu    ----    %llu\n", min_range, max_range);
 	
-	min_range = 0;
-	max_range = 0;
-
 	//unsigned long long 
 	min_range = 0;
-	max_range = pow2(sizeof(unsigned long long) * 8);
+	max_range = pow2(sizeof(unsigned long long) * 8) - 1;
 
 	printf("UNSIGNED LONG LONG : %llu    ----    %llu\n", min_range, max_range);
-
-	min_range = 0;
-	max_range = 0;
 
 	return 0;
 }  
