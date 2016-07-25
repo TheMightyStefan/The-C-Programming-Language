26191171c48e978417c99f73a6a093f2031725bc
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-07-25 21:49:19 +0300

    delete() and search() added

--- 2-4.c
+++ 2-4.c
@@ -2,23 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 
-void squeeze(char str1[], char str2[])
-{
-    int index_new_str = 0, index_str2 = 0;
+int search(char str1[], char character_to_search_for) {
+
+    int found = 0;
     for (int index_str1 = 0; index_str1 < strlen(str1); index_str1++) {
-        int delete_this_char = 0;
-        for (index_str2 = 0; index_str2 < strlen(str2); index_str2++) {
-            if (str1[index_str1] == str2[index_str2]) {
-                delete_this_char = 1;
-            }
+            if (str1[index_str1] == character_to_search_for) 
+                found = 1;
         }
-        if (!delete_this_char) {
-            str1[index_new_str++] = str1[index_str1];
+    if (found) 
+        return 1;
+    else 
+        return 0;   
+}
+         
+void delete(char str[], char char_to_delete) {
+    
+    int index_after_delete = 0;
+    for (int index_str = 0; index_str < strlen(str); index_str++) {
+        if (!(str[index_str] == char_to_delete)) {
+            str[index_after_delete++] = str[index_str];
         }
     }
-    str1[index_new_str] = '\0';
-}
+    str[index_after_delete] = '\0';
+}       
 
+void squeeze(char str1[], char str2[]) {
+
+    for (int index_str2 = 0; index_str2 < strlen(str2); index_str2++) {
+        if (search(str1, str2[index_str2])) {
+            delete(str1, str2[index_str2]);
+        }
+    }
+}
 int main()
 {
     char str1[50], str2[50];
