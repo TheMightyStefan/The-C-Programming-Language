fa097b9f8458d1555f76558a66b4eab2faab456d
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-07-26 15:31:21 +0300

    Fixed

--- 3-2.c
+++ 3-2.c
@@ -8,30 +8,32 @@ void escape(char modified_string[], char original_string[]) {
     for (int index = 0; index <= strlen(original_string); index++) {
          switch(original_string[index]) {
             case '\n' : 
-                modified_string[new_index++] = '\\';
-                modified_string[new_index] = 'n';
+                modified_string[new_index] = '\\';
+                modified_string[new_index + 1] = 'n';
+                new_index += 2;
                 break;
             case '\t' :
-                modified_string[new_index++] = '\\';
-                modified_string[new_index] = 't';
+                modified_string[new_index] = '\\';
+                modified_string[new_index + 1] = 't';
+                new_index += 2;
                 break;
             default :
                 modified_string[new_index] = original_string[index];
-        } 
-    new_index++;  
+                new_index++;
+                break;
+        }   
     }
     modified_string[new_index] = '\0';
 }
 
 int main () {
 
-    char modified_string[100], original_string[100];
-    printf("\n\nGive me the string : ");
-    scanf("%s", original_string);
+    char modified_string[100],
+         original_string[100] = "abcdef\tadafa\nhahaha\n";
 
     escape(modified_string, original_string);
 
-    printf("\nHere is my version : \n\n %s", modified_string);
+    printf("\nHere is my version : \n\n%s\n\n", modified_string);
     return 0;
 }
 
