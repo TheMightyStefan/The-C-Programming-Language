e1153bb0996db20102f96a3147dd511ea7ade745
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-07-26 15:56:35 +0300

    FIXED

--- 3-3.c
+++ 3-3.c
@@ -6,29 +6,29 @@
 void expand(char shorthand[], char complete[]) {
 
     int complete_index = 0;
-    for (int index = 0; index < strlen(shorthand); index++) {
+    for (int index = 0; index < strlen(shorthand) - 2; index++) {
         if (isalpha(shorthand[index]) && shorthand[index + 1] == '-' && isalpha(shorthand[index + 2])) {
             int FIRST_ASCII = (int)shorthand[index],
                 LAST_ASCII = (int)shorthand[index + 2];
-            while(FIRST_ASCII < LAST_ASCII) {
+            while(FIRST_ASCII <= LAST_ASCII) {
                 complete[complete_index] = (char)FIRST_ASCII;
-                FIRST_ASCII++;
                 complete_index++;
+                FIRST_ASCII++;
             }
         } else if (isdigit(shorthand[index]) && isdigit(shorthand[index + 2]) && shorthand[index + 1] == '-') {
             int FIRST_ASCII = (int)shorthand[index],
                 LAST_ASCII = (int)shorthand[index + 2];
-            while(FIRST_ASCII < LAST_ASCII) {
+            while(FIRST_ASCII <= LAST_ASCII) {
                 complete[complete_index] = (char)FIRST_ASCII;
-                FIRST_ASCII++;
                 complete_index++;
+                FIRST_ASCII++;
             }
-
         } else if (shorthand[index] != '-') {
             complete[complete_index] = shorthand[index];
             complete_index++;
         }
-    } 
+    }
+    complete[complete_index] = '\0'; 
 }
 
 int main () {
