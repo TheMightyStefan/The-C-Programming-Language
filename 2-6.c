981379a813c37c325c384387dd74a26d00948f60
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-07-26 23:21:53 +0300

    Added

--- 2-6.c
+++ 2-6.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int getbits(int y, int n) {
+void showbits(int number) {
     
-    return y & n;
+    while (number) {
+        if (number & 1) 
+            printf("1");
+        else
+            printf("0");
+        number >>= 1;
+    }
+    printf("\n");
 }
 
-int setbits(int x, p, n, y) {
+int getbits(int y, int n) {
+
+    return (y & (~0u >> ((sizeof(y) * 8 ) - n)));
+}
+
+int main() {
+    
+    int y = 144;
+    showbits(y);
+    showbits(getbits(y,3));
+    return 0;
+}
     
     
