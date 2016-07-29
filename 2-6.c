7a43d97d9158c4ff0fd52382dfb7345b1d3466b3
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-07-29 22:05:01 +0300

    Revert "Finished"
    This reverts commit e694c60f91bc1ffca9875a47b558bbeceab40e5d.
    
    Conflicts:
    	2-6.c

--- 2-6.c
+++ 2-6.c
@@ -1,16 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+<<<<<<< HEAD
 int setbits(int x, int p, int n, int y) {
     
     return x & ~(~(~0 << n) << (p+1-n)) | ((y & ~(~0<<n)) << (p+1-n));
+=======
+void showbits(int number) {
+    
+    int binary[100], index = 0;
+    while (number) {
+        if (number & 0x1) 
+            binary[index] = 1;
+        else
+            binary[index] = 0;
+        number >>= 1;
+        index++;
+    }
+    printf("\n");
+    for ( ; index >= 0; index--) {
+        printf("%d", binary[index]);
+    }
+    printf("\n");
+}
+
+int getbits(int y, int n) {
+
+    return (y & (~0u >> ((sizeof(y) * 8 ) - n)));
+>>>>>>> parent of e694c60... Finished
 }
 
 int main() {
     
+<<<<<<< HEAD
     int x = 150;
     int y = 85;
     printf("\nx = %x\ny = %x\n", x, y);
     printf("\nx final = %x\n", setbits(x, 3, 3, y));
+=======
+    int y = 453; 
+    showbits(64);
+    showbits(86);
+    showbits(43);
+    showbits(432);
+    showbits(getbits(y,3));
+>>>>>>> parent of e694c60... Finished
     return 0;
 }
