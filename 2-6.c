e6a11172d35ede16841f2e9c42d8c0c8fde3640e
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-07-29 23:06:29 +0300

    Added explications(comments); Removed printf()'s

--- 2-6.c
+++ 2-6.c
@@ -3,20 +3,32 @@
 
 int getbits(int y, int n) {
 
+    // We get the last n bits of y
     return (y & (~0u >> ((sizeof(y) * 8 ) - n)));
 }
 
 int setbits(int x, int p, int n, int y) {
     
-    printf("\nSizeof x : %d\n", sizeof(x));
+    // We make a copy of x for later uses.
     int z = x;
+
+    // We get the last significant bit to be the one 
+    // on the position p + n - 1, the last one we need to change. 
     x = x >> ((sizeof(x) * 2) - p - n - 1);
-    printf("\nx >> %ld = %x\n", (sizeof(x) * 2) - p - n - 1, x);
+
+    // We change the last n bits of x after the shift 
+    // with the last n bits of y.
     x = (x & ~n) | getbits(y, n);
-    printf("\nx + noii biti : %x\n", x);
+    
+    // We shift x back to get the most significant bit on the 
+    // initial position.
     x <<= ((sizeof(x) * 2) - p - n - 1);
-    printf("\nx << %ld = %x\n", (sizeof(x) * 2) - p - n - 1, x);
+    
+    // We change the last bits of x obtained after the shift
+    // with the last bits of the initial x.
     x = (x & (~(sizeof(x) * 2) - p - n - 1)) | getbits(z, sizeof(x) * 2 - p - n - 1);
+    
+    // We return the needed x.
     return x;
 }
 
