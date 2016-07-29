c038b3a4de2a912954da2285a734087150bda8ef
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-07-29 21:20:35 +0300

    Fixed identation of line #6

--- 2-6.c
+++ 2-6.c
@@ -3,7 +3,7 @@
 
 int setbits(int x, int p, int n, int y) {
     
-    return x & ~(~(~0 << n) << (p+1-n)) | (( y & ~(~0<<n)) << (p+1-n));
+    return x & ~(~(~0 << n) << (p+1-n)) | ((y & ~(~0<<n)) << (p+1-n));
 }
 
 int main() {
