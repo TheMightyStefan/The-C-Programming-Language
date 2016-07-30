0f2c0b45e66c960624c3499658ac34d6dedbdf26
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-07-30 11:02:25 +0300

    Fixed the overall algorithm.Added explications

--- 2-7.c
+++ 2-7.c
@@ -3,13 +3,43 @@
 
 int invert(int x, int p, int n) {
 
-    return x ^ (~(~0 << n) << (p + 1 - n));
+    // We make a copy of x for later use.
+    int z = x;
+    
+    // We get the bit on position p to be 
+    // the least significant one.
+    x = x >> p;
+
+    // We set the last n bits to 0.
+    x = (x >> n) << n;
+
+    // We change the last n bits with the 
+    // inverted corespondent ones from the copy
+    // of the original x.
+    x = x | ~((~0 << n) | ((z >> p) & (~(~0 << n))));
+
+    // We shift x back to get the most significant 
+    // bit on the initial position.
+    x = x << p;
+    
+    // We set the last p bits of x to 0.
+    x = (x >> p) << p;
+
+    // We change the last p bits of x
+    // with the last p bits from the copy 
+    // of the original x.
+    x = x | ((z >> p) & (~(~0 << p)));
+
+    // We return the needed x.
+    return x;  
 }
 
 int main() {
 
-    int x = 150;
+    int x = 255;
+    int p = 2;
+    int n = 2;
     printf("\nx = %x\n", x);
-    printf("\nInverted version : %x\n", invert(x, 2, 2));
+    printf("\nInverted version : %x\n", invert(x, p, n));
     return 0;
 }
