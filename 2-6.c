83f08faf22f5cdb01bc7b63af93a53edc8ee2330
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-07-30 10:20:49 +0300

    Fixed the overall algorithm : Changed the getbits() formula, Changed setbits(); Fixed the explications

--- 2-6.c
+++ 2-6.c
@@ -4,29 +4,29 @@
 int getbits(int y, int n) {
 
     // We get the last n bits of y
-    return (y & (~0u >> ((sizeof(y) * 8 ) - n)));
+    return y & (~(~0 << n));
 }
 
 int setbits(int x, int p, int n, int y) {
     
-    // We make a copy of x for later uses.
+    // We make a copy of x for later use.
     int z = x;
 
-    // We get the last significant bit to be the one 
-    // on the position p + n - 1, the last one we need to change. 
-    x = x >> ((sizeof(x) * 2) - p - n - 1);
+    // We get the bit on the position p to be the last  
+    // significant one. 
+    x = x >> p;
 
     // We change the last n bits of x after the shift 
     // with the last n bits of y.
-    x = (x & ~n) | getbits(y, n);
+    x = ((x >> n) << n) | getbits(y, n);
     
     // We shift x back to get the most significant bit on the 
     // initial position.
-    x <<= ((sizeof(x) * 2) - p - n - 1);
+    x = x << p;
     
     // We change the last bits of x obtained after the shift
     // with the last bits of the initial x.
-    x = (x & (~(sizeof(x) * 2) - p - n - 1)) | getbits(z, sizeof(x) * 2 - p - n - 1);
+    x = ((x >> p) << p) | getbits(z, p);
     
     // We return the needed x.
     return x;
@@ -34,10 +34,12 @@ int setbits(int x, int p, int n, int y) {
 
 int main() {
     
-    int x = 150;
-    int y = 85;
+    int y = 0;
+    int n = 4;
+    int p = 3;
+    int x = 255;
     printf("\nx = %x\ny = %x\n", x, y);
-    printf("\nx final = %x\n", setbits(x, 3, 3, y));
+    printf("\nx final = %x\n", setbits(x, p, n, y));
     return 0;
 }
     
