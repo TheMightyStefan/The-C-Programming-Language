690ab72435a890613a2a2bef306001d1609b2d6f
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-07-31 21:08:47 +0300

    Improved indentation

--- 3-1.c
+++ 3-1.c
@@ -3,10 +3,10 @@
 
 int binsearch(int x, int v[], int n) {
     
-    int low, high, mid;
-    
-    low = 0;
-    high = n - 1;
+    int mid = 0; 
+    int low = 0;
+    int high = n - 1;
+
     while (low <= high && x != v[mid]) {
         mid = (low + high) / 2;
         if (x < v[mid]) 
@@ -14,6 +14,7 @@ int binsearch(int x, int v[], int n) {
         else  
             low = mid + 1;
     }
+
     if (x == v[mid]) 
         return mid;
     else 
@@ -29,5 +30,6 @@ int main() {
         printf("\n%d\n", binsearch(target, array, 9));
     else 
         printf("\nNot Found\n");
+
     return 0;
-}    
+}      
