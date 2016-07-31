bd0c4340e7bf338873e867736f5913e82f5647f4
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-07-31 20:58:59 +0300

    Tried to modify binsearch() to perform minimum tests

--- 3-1.c
+++ 3-1.c
@@ -7,14 +7,17 @@ int binsearch(int x, int v[], int n) {
     
     low = 0;
     high = n - 1;
-    while (low <= high) {
+    while (low <= high && x != v[mid]) {
         mid = (low + high) / 2;
-        if (x != v[mid]) 
-            (x < v[mid]) ? (high = mid - 1) : (low = mid + 1);
-        else 
-            return mid;
+        if (x < v[mid]) 
+            high = mid - 1;
+        else  
+            low = mid + 1;
     }
-    return -1;
+    if (x == v[mid]) 
+        return mid;
+    else 
+        return -1;
 }
 
 int main() {
