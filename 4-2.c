851baeeddd25d5a3b1b002198d2db3c237c17f61
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-08-15 20:45:15 +0300

    Added initial version of 4-2.c. Temporarely not working properly

---
+++ 4-2.c
@@ -0,0 +1,73 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+double power_up(int base, int power) {
+
+	double result = 0;
+	
+	if (power == 0) 
+		return 1;
+	
+	while (power > 0) { 
+		result = result * base;
+		power--;
+	}	
+
+	return result;
+}
+
+double string_to_double(char string[]) {
+	
+	double power, value;
+	int index, sign = 0;
+	double e_power = 0;
+	int e_index = 0;
+	int e_found = 0;
+
+	for (index = 0; isspace(string[index]); index++);
+
+	sign = (string[index] == '-') ? -1 : 1;
+
+	if (string[index] == '+' || string[index] == '-')
+		index++;
+
+	for (value = 0.0; isdigit(string[index]); index++)
+		value = 10.0 * value + (string[index] - '0');
+
+	if (string[index] == '.')
+		index++;
+
+	for (int new_index = index; new_index < strlen(string); new_index++) {
+		if (string[new_index] == 'e' && string[new_index] == 'E') {
+			e_found = 1;
+			e_index = new_index;
+		}	
+	}
+	
+	if (e_found) {
+		for (int new_index = e_index + 2; new_index < strlen(string); new_index++)
+			e_power = e_power * 10.0 + (string[new_index] - '0'); 
+	}
+
+	for (power = 1.0; isdigit(string[index]); index++) {
+		value = 10.0 * value + (string[index] - '0');
+		power *= 10.0;
+	}
+	printf("\n%d\n", e_found);
+	return sign * value / power * power_up(10, e_power);
+}
+
+int main() {
+
+	char string[100];
+
+	printf("\nYour string : ");
+	scanf("%s", string);
+	printf("\nYour double : %g\n", string_to_double(string));
+
+	return 0;
+}
+
+
+		
