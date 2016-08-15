8fe0565cdc647bce9602964b821240a00b9319a2
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-08-15 20:44:12 +0300

    Changed the signed declaration to unsigned

--- 4-1.c
+++ 4-1.c
@@ -4,8 +4,8 @@
 int strrindex(char string[], char target[]) {
 
     for (int index = strlen(string) - 1; index >= 0; index--) { 
-        signed int string_index = index;
-        signed int target_index = strlen(target) - 1;
+        int string_index = index;
+        int target_index = strlen(target) - 1;
         int search_length = 0;
         while (string[string_index] == target[target_index] && string_index >= 0) {
             search_length++;
