537ddae9de04fcdfd8bd214fd2e0af4c81feec15
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-08-16 19:13:52 +0300

    Improved algorithm in 4-2.c : Removed e_found, e_index and after_dot_index

--- 4-2.c
+++ 4-2.c
@@ -4,20 +4,20 @@
 
 double power_up(int base, int power) {
 
-        double result = 1;
+    double result = 1;
 	
 	if (power == 0) 
-		return 1;
-	else if (power < 0) 
-		while (power < 0) {
-			result /= base;
-			power++;
-		}		
-	else 
-		while (power > 0) { 
-			result *= base;
-			power--;
-		}	
+        return 1;
+
+	while (power < 0) { 
+        result /= base;
+		power++;
+	}	
+
+	while (power > 0) { 
+		result *= base;
+		power--;
+	}	
 
 	return result;
 }
@@ -36,40 +36,30 @@ double string_to_double(char string[]) {
 	double value = 0;
 
 	while (isdigit(string[index])) {
-		value = 10.0 * value + (string[index] - '0');
-                index++;
-        }
+	    value = 10.0 * value + (string[index] - '0');
+        index++;
+     }
 
 	if (string[index] == '.')
 		index++;
 
-        int e_index;
-        int e_found = 0;
-
-	for (int after_dot_index = index; after_dot_index < strlen(string); after_dot_index++) {
-		if (string[after_dot_index] == 'e' || string[after_dot_index] == 'E') {
-			e_found = 1;
-			e_index = after_dot_index;
-		}	
-	}
-
-    	double e_power = 0;
-
-	if (e_found) {
-		for (int after_e_index = e_index + 2; after_e_index < strlen(string); after_e_index++)
-			e_power = e_power * 10 + (string[after_e_index] - '0');
-	
-		e_power *= (string[e_index + 1] == '-') ? -1 : 1;
-	}
-
-        double power;
+    double power = 1;
 	
 	for (power = 1.0; isdigit(string[index]); index++) {
-                value = 10.0 * value + (string[index] - '0');
-                power *= 10.0;
-        }
-	
-        return sign * value / power * power_up(10, e_power);
+        value = 10.0 * value + (string[index] - '0');
+        power *= 10.0;
+    }
+
+    double e_power = 0;
+
+    if (string[index] == 'e' || string[index] == 'E') {
+        for (int after_e_index = index + 2; after_e_index < strlen(string); after_e_index++)
+            e_power = e_power * 10 + (string[after_e_index] - '0');
+
+        e_power *= (string[index + 1] == '-') ? -1 : 1;                       
+    }    
+
+    return sign * value / power * power_up(10, e_power);
 }
 
 int main() {
