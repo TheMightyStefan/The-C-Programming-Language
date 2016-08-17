31f1348a8471da1ccf58e1704a8a5d72d30cb442
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-08-17 15:41:16 +0300

    Removed tabs in 4-2

--- 4-2.c
+++ 4-2.c
@@ -17,30 +17,30 @@ double string_to_double(char string[]) {
 	double value = 0;
 
 	while (isdigit(string[index])) {
-	    value = 10.0 * value + (string[index] - '0');
-        index++;
-     }
+		value = 10.0 * value + (string[index] - '0');
+		index++;
+	}
 
 	if (string[index] == '.')
 		index++;
 
-    double power = 1;
+	double power = 1;
 	
 	for (power = 1.0; isdigit(string[index]); index++) {
-        value = 10.0 * value + (string[index] - '0');
-        power *= 10.0;
-    }
+		value = 10.0 * value + (string[index] - '0');
+		power *= 10.0;
+	}
 
-    double e_power = 0;
+	double e_power = 0;
 
-    if (string[index] == 'e' || string[index] == 'E') {
-        for (int after_e_index = index + 2; after_e_index < strlen(string); after_e_index++)
-            e_power = e_power * 10 + (string[after_e_index] - '0');
+	if (string[index] == 'e' || string[index] == 'E') {
+		for (int after_e_index = index + 2; after_e_index < strlen(string); after_e_index++)
+			e_power = e_power * 10 + (string[after_e_index] - '0');
 
-        e_power *= (string[index + 1] == '-') ? -1 : 1;                       
-    }    
+		e_power *= (string[index + 1] == '-') ? -1 : 1;                       
+	}    
 
-    return sign * value / power * pow(10, e_power);
+	return sign * value / power * pow(10, e_power);
 }
 
 int main() {
