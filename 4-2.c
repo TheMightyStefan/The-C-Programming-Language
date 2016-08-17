4b184ae923de750b1a0fea5b0bc70d93c711c7f5
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-08-17 15:51:46 +0300

    Replaced tabs with spaces in 4-2

--- 4-2.c
+++ 4-2.c
@@ -5,53 +5,53 @@
 
 double string_to_double(char string[]) {
 	
-	int index;
+    int index;
 
-	for (index = 0; isspace(string[index]); index++);
+    for (index = 0; isspace(string[index]); index++);
 
-	int sign = (string[index] == '-') ? -1 : 1;
+    int sign = (string[index] == '-') ? -1 : 1;
 
-	if (string[index] == '+' || string[index] == '-')
-		index++;
+    if (string[index] == '+' || string[index] == '-')
+        index++;
 
-	double value = 0;
+    double value = 0;
 
-	while (isdigit(string[index])) {
-		value = 10.0 * value + (string[index] - '0');
-		index++;
-	}
+    while (isdigit(string[index])) {
+        value = 10.0 * value + (string[index] - '0');
+        index++;
+    }
 
-	if (string[index] == '.')
-		index++;
+    if (string[index] == '.')
+        index++;
 
-	double fractional_part = 1;
+    double fractional_part = 1;
 	
-	for (fractional_part = 1.0; isdigit(string[index]); index++) {
-		value = 10.0 * value + (string[index] - '0');
-		fractional_part *= 10.0;
-	}
+    for (fractional_part = 1.0; isdigit(string[index]); index++) {
+        value = 10.0 * value + (string[index] - '0');
+        fractional_part *= 10.0;
+    }
 
-	double e_power = 0;
+    double e_power = 0;
 
-	if (string[index] == 'e' || string[index] == 'E') {
-		for (int after_e_index = index + 2; after_e_index < strlen(string); after_e_index++)
-			e_power = e_power * 10 + (string[after_e_index] - '0');
+    if (string[index] == 'e' || string[index] == 'E') {
+        for (int after_e_index = index + 2; after_e_index < strlen(string); after_e_index++)
+            e_power = e_power * 10 + (string[after_e_index] - '0');
 
-		e_power *= (string[index + 1] == '-') ? -1 : 1;                       
-	}    
+        e_power *= (string[index + 1] == '-') ? -1 : 1;                       
+    }    
 
-	return sign * value / fractional_part * pow(10, e_power);
+    return sign * value / fractional_part * pow(10, e_power);
 }
 
 int main() {
 
-	char string[100];
+    char string[100];
 
-	printf("\nYour string : ");
-	scanf("%s", string);
-	printf("\nYour double : %g\n", string_to_double(string));
+    printf("\nYour string : ");
+    scanf("%s", string);
+    printf("\nYour double : %g\n", string_to_double(string));
 
-	return 0;
+    return 0;
 }
 
 
