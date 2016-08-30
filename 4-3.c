3608930cce8c56a9343edd5b995a4a5df67aede0
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-08-30 20:24:51 +0300

    Added comments in 4-3

--- 4-3.c
+++ 4-3.c
@@ -12,13 +12,15 @@ double stack[MAX_STACK_SIZE];
 char buffer[BUFFER_SIZE];
 int buffer_index = 0;
 
+// Function for pushing into stack
 void push(double new_stack_number) {
     if (stack_index < MAX_STACK_SIZE)
         stack[stack_index++] = new_stack_number;
     else
         printf("Error : Stack full\n");
 }
-    
+
+//Function for poping from stack
 double pop() { 
     if (stack_index > 0) 
         return stack[--stack_index];
@@ -28,10 +30,12 @@ double pop() {
     }
 }
 
+//Function for getting input characters
 int get_character() {
     return (buffer_index > 0) ? buffer[--buffer_index] : getchar();
 }
 
+//Function to "undo" get_character()
 void unget_character(int character) {
     if (buffer_index >= BUFFER_SIZE)
         printf("Ungetch : Too many characters\n");
@@ -39,21 +43,26 @@ void unget_character(int character) {
         buffer[buffer_index++] = character;
 }
 
+//Function to get the type of input : Number / Operators
 int get_operators(char input_string[]) {
     int string_index = 0;
     int character;
 
+    //Ignore the spaces beetween characters
     while ((input_string[0] = character = get_character()) == ' ' || character == '\t');
 
     input_string[1] = '\0';
 
+    //Return the operator, except in case of '-'
     if (!isdigit(character) && character != '.' && character != '-')
         return character;
 
+    //In case of positive numbers
     if (character != '-') {
+        //Get the integer part
         if (isdigit(character))
         	while (isdigit(input_string[++string_index] = character = get_character()));
-
+        //Get the fractional part
     	if (character == '.')
         	while (isdigit(input_string[++string_index] = character = get_character()));
     
@@ -64,14 +73,18 @@ int get_operators(char input_string[]) {
     
     	return NUMBER;
    
+   //In case of negative numbers / subtraction
    } else {
 	   int temp_character;
 	   if (isdigit(temp_character = get_character())) {
-		input_string[++string_index] = temp_character;
+		//Negative number
+                input_string[++string_index] = temp_character;
 		input_string[0] = '-';
-
+        
+                //Get integer part
 		while (isdigit(input_string[++string_index] = character = get_character()));
 
+                //Get fractional part
 		if (character == '.')
 			while (isdigit(input_string[++string_index] = character = get_character()));
 
@@ -82,6 +95,7 @@ int get_operators(char input_string[]) {
 		}
 		return NUMBER;
 	   } else {
+                //Subtraction
 		unget_character(temp_character);
 		return '-';
 	   }
@@ -95,10 +109,12 @@ int main() {
     char input_string[MAX_OPERATORS];
 
     while ((type = get_operators(input_string)) != EOF) {
-        switch(type) {
+           switch(type) {
+            //Push the number into the stack
             case NUMBER:
                 push(atof(input_string));
                 break;
+            //Do the requested operations
             case '+':
                 push(pop() + pop());
                 break;
@@ -124,6 +140,7 @@ int main() {
                     printf("Error : 0 divisor\n");
                 break;
             case '\n':
+                //Print output
 		printf("\t%g\n", pop());
                 break;
             default:
