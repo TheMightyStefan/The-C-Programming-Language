eca8f5f5c23ff21c7eddf94fcb995188a0c3abb3
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-09-03 13:22:38 +0300

    Added 4-4

---
+++ 4-4.c
@@ -0,0 +1,175 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+// Command to print the top element of the stack
+#define TOP_STACK_ELEMENT 'P'
+
+// Comand to swap the top two elements
+#define SWAP_ELEMENTS 'S'
+
+// Command to duplicate the top element of the stack
+#define DUPLICATE 'D'
+
+// Command to clear the stack
+#define CLEAR_STACK 'C'
+
+#define MAX_OPERATORS 100
+#define NUMBER '0'
+#define MAX_STACK_SIZE 100
+#define BUFFER_SIZE 100
+
+int stack_index = 0;
+double stack[MAX_STACK_SIZE];
+char buffer[BUFFER_SIZE];
+int buffer_index = 0;
+
+void clear_stack() {
+    stack_index = 0;
+}
+// Function for pushing into stack
+void push(double new_stack_number) {
+    if (stack_index < MAX_STACK_SIZE)
+        stack[stack_index++] = new_stack_number;
+    else
+        printf("Error : Stack full\n");
+}
+
+// Function for poping from stack
+double pop() {
+    if (stack_index > 0)
+        return stack[--stack_index];
+    else {
+        printf("Error : Stack empty\n");
+        return 0;
+    }
+}
+
+// Function for getting input characters
+int get_character() {
+    if (buffer_index > 0)
+        return  buffer[--buffer_index];
+    else
+        return getchar();
+}
+
+// Function to "undo" get_character()
+void unget_character(int character) {
+    if (buffer_index >= BUFFER_SIZE)
+        printf("Ungetch : Too many characters\n");
+    else
+        buffer[buffer_index++] = character;
+}
+
+// Function to get the digits of a number
+void get_digits(char string[]) {
+    int string_index = 0;
+
+    do {
+        string[++string_index] = get_character();
+    } while (isdigit(string[string_index]) || string[string_index] == '.');
+
+    if (string[string_index] != EOF)
+        unget_character(string[string_index]);
+
+    string[string_index] = '\0';
+}
+
+// Function to get the type of input : Number / Operators
+int get_operators(char input_string[]) {
+    int character = get_character();
+
+    // Ignore the spaces beetween characters
+    while (isspace(character) && character != '\n')
+        character = get_character();
+
+    // Return the operator, except in case of '-'
+    if (!isdigit(character) && character != '.' && character != '-')
+        return character;
+
+    if (character != '-') {
+        input_string[0] = character;
+        get_digits(input_string);
+        return NUMBER;
+    } else if (stack_index < 2) {
+        input_string[0] = '-';
+        get_digits(input_string);
+        return NUMBER;
+    } else {
+        return '-';
+    }
+}
+
+int main() {
+    int type;
+
+    do {
+        double first_operand;
+        double second_operand;
+        char input_string[MAX_OPERATORS];
+        type = get_operators(input_string);
+        switch (type) {
+            // Push the number into the stack
+            case NUMBER:
+                push(atof(input_string));
+                break;
+            // Do the requested operations
+            case '+':
+                push(pop() + pop());
+                break;
+            case '*':
+                push(pop() * pop());
+                break;
+            case '-':
+                second_operand = pop();
+                first_operand = pop();
+                push(first_operand - second_operand);
+                break;
+            case '/':
+                second_operand = pop();
+                first_operand = pop();
+                if (second_operand != 0)
+                    push(first_operand / second_operand);
+                else
+                    printf("Error : 0 divisor\n");
+                break;
+            case '%':
+                second_operand = pop();
+                first_operand = pop();
+                if (second_operand != 0)
+                    push((int)first_operand % (int)second_operand);
+                else
+                    printf("Error : 0 divisor\n");
+                break;
+            case 'P':
+                first_operand = pop();
+                printf("\t%.8g", first_operand);
+                push(first_operand);
+                break;
+            case 'D': 
+                first_operand = pop();
+                push(first_operand);
+                push(first_operand);
+                break;
+            case 'S':
+                first_operand = pop();
+                second_operand = pop();
+                push(first_operand);
+                push(second_operand);
+                break;
+            case 'C' :
+                clear_stack();
+                break;
+            // Print output
+            case '\n':
+                printf("\t%g\n", pop());
+                break;
+            default:
+                printf("Error : Unknown command %s\n", input_string);
+                break;
+        }
+    } while (type != EOF);
+
+    return 0;
+}
+
