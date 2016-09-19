821bac143b521ccb3838bdad3b840d36df6a256e
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-09-19 16:33:01 +0300

    4-7 working properly

--- 4-7.c
+++ 4-7.c
@@ -1,8 +1,17 @@
+#include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
 #define BUFFER_SIZE 100
 
-char buffer[];
+char buffer[100];
 int buffer_index = 0;
+
+int get_character() {
+        if (buffer_index > 0)
+                return buffer[--buffer_index];
+        else
+                return getchar();
+}
 
 void unget_character(int character) {
         if (buffer_index >= BUFFER_SIZE)
@@ -19,5 +28,23 @@ void unget_string(char string[]) {
 }
 
 int main() {
-        int string[]
+        char character;
+        char string[100];
+        int index = 0;
+        
+        character = get_character();
+
+        do {
+            string[++index] = character;
+            character = get_character();
+        } while (character != '\n');
+
+        unget_string(string);
+
+        do {
+            character = get_character();
+            printf("%c", character);
+        } while (character != '\n');
+
+        return 0;
 }
