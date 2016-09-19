58b5e1b29ac6d0646a6582c89a4d0a4646f16199
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-09-19 17:09:45 +0300

    Changed 8 spaces identation to 4 spaces 4-7

--- 4-7.c
+++ 4-7.c
@@ -7,36 +7,35 @@ char buffer[100];
 int buffer_index = 0;
 
 void unget_character(int character) {
-        if (buffer_index >= BUFFER_SIZE)
-                printf("\nUngetch : Too many characters");
-        else
-                buffer[buffer_index++] = character;
+    if (buffer_index >= BUFFER_SIZE)
+        printf("\nUngetch : Too many characters");
+    else
+        buffer[buffer_index++] = character;
 }
 
 int get_character() {
-        if (buffer_index > 0)
-                return buffer[--buffer_index];
-        else
-                return getchar();
+    if (buffer_index > 0)
+        return buffer[--buffer_index];
+    else
+        return getchar();
 }
 
 void unget_string(char string[]) {
-        int size = strlen(string);
+    int size = strlen(string);
 
-        while (size > 0)
-                unget_character(string[--size]);
+    while (size > 0)
+        unget_character(string[--size]);
 }
 
 int main() {
-        
-        unget_string("abcdef");
+    unget_string("abcdef");
 
-        char character;
+    char character;
 
-        do {
-                character = get_character();
-                printf("%c", character);
-        } while (character != 'f');
+    do {
+        character = get_character();
+        printf("%c", character);
+    } while (character != 'f');
 
-        return 0;
+    return 0;
 }
