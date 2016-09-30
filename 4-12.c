0d669cb642cf43e4e2d82885f16c6eb2269eb569
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-09-30 20:17:44 +0300

    Added itoa2_operations() and modified itoa2() 4-12

--- 4-12.c
+++ 4-12.c
@@ -20,25 +20,18 @@ void itoa(int number, char digit_string[]) {
     digit_string[index] = '\0';
 }
 
-void itoa2(int number, char digit_string[], int index) {
-   if (number < 0) {
-        digit_string[0] = '-';
-        number = -number;
-   }
-
-   if (digit_string[0] != '-')
-        digit_string[0] = '+';
-
-   if (number / 10) {
+void itoa2_operation(int number, char digit_string[], int index, int sign) {
+    if (number / 10) {
         index--;
-        itoa2(number / 10, digit_string, index);
-    }
-    else 
-        index = 1;
+        itoa2_operation(number / 10, digit_string, index, sign);
+    } else 
+        index = sign;
 
-    digit_string[index++] = abs(number) % 10 + '0';
+    printf("\n%d -> %c", index, digit_string[index]);
 
-    digit_string[index] = '\0';
+    digit_string[index] = abs(number) % 10 + '0';
+
+    digit_string[++index] = '\0';
 }
 
 int digit_counter(int number) {
@@ -57,11 +50,20 @@ int digit_counter(int number) {
     return digits;
 }
 
+void itoa2(int number, char digit_string[]) {
+    if (number < 0) {
+        digit_string[0] = '-';
+        number = -number;
+        itoa2_operation(number, digit_string, digit_counter(number), 1);
+    } else
+        itoa2_operation(number, digit_string, digit_counter(number), 0);
+}
+
 int main() {
-    int number = 24321;
+    int number = -221;
     char digit_string[100];
 
-    itoa2(number, digit_string, digit_counter(number));
+    itoa2(number, digit_string);
 
     printf("\n%s", digit_string);
 
