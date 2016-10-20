0bb2a1b1ad3eec9fb2a32d9809771e4ef691d665
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-10-20 20:27:56 +0300

    Added string input method 5-2

--- 5-2.c
+++ 5-2.c
@@ -1,87 +1,61 @@
 #include <ctype.h>
 #include <stdio.h>
 
-#define BUFFER_SIZE 100
-
-int buffer_index = 0;
-int buffer[100];
-
-void unget_character(int character) {
-    if (buffer_index >= BUFFER_SIZE)
-        printf("\nUngetch : Too many characters");
-    else
-        buffer[buffer_index++] = character;
-}
-
-int get_character() {
-    if (buffer_index > 0)
-        return buffer[--buffer_index];
-    else
-        return getchar();
-}
-
-int remove_spaces() {
-    char character = get_character();
-
-    while (isspace(character) && character != '\n')
-        character = get_character();
-
-    if (character == '\n')
-        return 1;
-
-    unget_character(character);
+int remove_spaces(char string[], int *index) {
+    while (isspace(string[*index]) && string[*index] != '\n')
+        *index++;
 
     return 0;
 }
 
-int get_float(double *pointer) {
-    char character = get_character();
-
-    if (!isdigit(character) && character != EOF && character != '+' && character != '-') {
-        unget_character(character);
+int get_float(double *pointer, char string[], int *index) {
+    if (!isdigit(string[*index]) && string[*index] != EOF && string[*index] != '+' && string[*index] != '-')
         return 0;
-    }
 
-    int sign = (character == '-') ? -1 : 1;
+    int sign = (string[*index] == '-') ? -1 : 1;
    
-    if (character == '+' || character == '-') {
-        int next_character = get_character();
-
-        if (isdigit(next_character)) {
-            character = next_character;
-        } else {
-            unget_character(next_character);
-            unget_character(character);
+    if (string[*index] == '+' || string[*index] == '-') {
+        if (isdigit(string[*index + 1])) {
+            *index++;
+        } else 
             return 0;
-        }
     }
 
-    for (*pointer = 0; isdigit(character); character = get_character())
-        *pointer = 10 * *pointer + (character - '0');
+    for (*pointer = 0; isdigit(string[*index]); *index++)
+        *pointer = 10 * *pointer + (string[*index] - '0');
 
     double power = 1;
 
-    if (character == '.') {
-        character = get_character();
+    if (string[*index] == '.') {
+        index++;
 
-        for (power = 1.0; isdigit(character); character = get_character()) {
-            *pointer = 10.0 * *pointer + (character - '0');
+        for (power = 1.0; isdigit(string[*index]); *index++) {
+            *pointer = 10.0 * *pointer + (string[*index] - '0');
             power *= 10.0;
         }
     }
     *pointer = *pointer / power * sign;
 
-    if (character != EOF)
-        unget_character(character);
-
     return 1;
 }
 
 int main() {
-    while (!remove_spaces()) {
-        double number;
+    char input[100];
+    double number;
+    int index = 0;
 
-        if (get_float(&number))
+    for (int index = 0; input[index] != EOF; index++) {
+        input[index] = getchar();
+    }
+
+    if (input[index] == '\n')
+        input[index] = '\0';
+
+    while (input[index] != '\0') {
+        printf("\n%d", index);
+
+        remove_spaces(input, &index);
+        if (get_float(&number, input, &index))
             printf("Float : %g\n", number);
     }
 
