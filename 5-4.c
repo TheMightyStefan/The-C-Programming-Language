b6fc804bf75dc2f2679438df7ff5e9e9b9c4e8e1
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-10-22 16:52:35 +0300

    Fixed identation 5-4

--- 5-4.c
+++ 5-4.c
@@ -5,12 +5,12 @@ int strend(char search[], char target[]) {
     int search_length = strlen(search) - 1;
     int target_length = strlen(target) - 1;
 
-    if (search_length < target_length || !target_length || !search_length)
+    if (search_length < target_length || target_length == 0 || search_length == 0)
         return 0;
 
     for ( ; target_length > -1; target_length--, search_length--) {
         if (*(target + target_length) != *(search + search_length))
-                return 0;
+            return 0;
     }
     
     return 1;
