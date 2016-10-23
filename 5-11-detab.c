5e2d5cc90ed167ccbc7b509893a991900c2af35e
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-10-23 20:12:49 +0300

    Fixed 5-11-detab

--- 5-11-detab.c
+++ 5-11-detab.c
@@ -1,16 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main (int argc, char **argv)
-{
+#define DEFAULT_TAB_STOP 4
+
+int main (int argc, char **argv) {
     char character;
-    int charsBehindTabStop = 0;
     int arg_index = 1;
+    int charsBehindTabStop = 0;
 
-    while ((character = getchar()) != EOF) {
-        int tabstop = atof(argv[arg_index]);
+    while ((character = getchar()) != '\n') {
+        int tabstop = 0;
 
-        printf("%d\n", tabstop);
+        if (arg_index > argc - 1)
+            tabstop = DEFAULT_TAB_STOP;
+        else {
+            tabstop = atof(argv[arg_index]);
+        }
+
         if (character == '\t') {
             while (charsBehindTabStop < tabstop) {
                 printf("*");
@@ -25,6 +31,7 @@ int main (int argc, char **argv)
             charsBehindTabStop = 0;
             arg_index++;
         }
+       
     }
 
     return 0;
