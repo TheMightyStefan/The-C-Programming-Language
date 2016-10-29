b044f7422baa740fd34983255dabc7fc4ce78674
TheMightyStefan <stefan.1.stefanache@gmail.com>
2016-10-29 17:26:58 +0300

    Minor modifications 5-11-detab

--- 5-11-detab.c
+++ 5-11-detab.c
@@ -1,28 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define DEFAULT_TAB_STOP 4
+#define DEFAULT_TABSTOP 4
 
 int main (int argc, char **argv) {
-    char character;
+    char character = getchar();
     int arg_index = 1;
     int charsBehindTabStop = 0;
 
-    while ((character = getchar()) != '\n') {
+    while (character != '\n') {
         int tabstop = 0;
 
         if (arg_index > argc - 1)
-            tabstop = DEFAULT_TAB_STOP;
-        else {
-            tabstop = atof(argv[arg_index]);
-        }
+            tabstop = DEFAULT_TABSTOP;
+        else
+            tabstop = atoi(argv[arg_index]);
 
-        if (character == '\t') {
+        if (character == '\t')
             while (charsBehindTabStop < tabstop) {
                 printf("*");
                 charsBehindTabStop++;
             }
-        } else {
+        else {
             printf("%c", character);
             charsBehindTabStop++;
         }
@@ -31,7 +30,8 @@ int main (int argc, char **argv) {
             charsBehindTabStop = 0;
             arg_index++;
         }
-       
+
+        character = getchar();
     }
 
     return 0;
