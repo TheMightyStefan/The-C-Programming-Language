76738968a96f25efac9ceb4aad5efe6b9f679491
TheMigthyStefan <stefan.1.stfanache@gmail.com>
2017-01-05 20:31:31 +0000

    Added 6-1.c

--- 6-1.c
+++ 6-1.c
@@ -26,22 +26,46 @@ int get_character() {
 
 int get_word(char *word, int limit) {
     char *word_ptr = word;
-    int character;
-
+    char character;
+    char temp_character;
+ 
     while (isspace(character = get_character()));
 
     if (character != EOF)
         *word_ptr++ = character;
 
     if (!isalpha(character)) {
+        if (character == '\"')
+            for (character = get_character(); character != '\"'; character = get_character());
+        else if (character == '#')
+            for (character = get_character(); character != '\n'; character = get_character());
+        else if (character == '/') {
+            if ((character = get_character()) == '/')
+                for (character = get_character(); character != '\n'; character = get_character());
+            else if (character == '*') {
+                for (character = get_character(), temp_character = get_character(); character != '*' && temp_character != '/'; character = get_character(), temp_character = get_character())
+                    unget_character(temp_character);
+            } else
+                unget_character(character);
+        } else 
+            for ( ; !isspace(character) && character != EOF; character = get_character());
+
+        if (character != '\"' && character != '\n' && character != '/')
+            unget_character(character);
+    
         *word_ptr = '\0';
-        return character;
+        return character; 
     }
 
     for ( ; --limit > 0; word_ptr++)
         if (!isalnum(*word_ptr = get_character())) {
-            unget_character(*word_ptr);
-            break;
+            if (!isspace(*word_ptr)) {
+                unget_character(*word_ptr);
+                return (*word_ptr);      
+            } else {
+                unget_character(*word_ptr);
+                break;
+            }
         }
 
     *word_ptr = '\0';
@@ -58,15 +82,17 @@ struct key {
     { "char", 0 },
     { "const", 0 },
     { "continue" , 0 },
+    // ........... //
     { "default", 0 },
-    { "unsigned", 0 },
+    { "define", 0 },
+    { "if", 0 },
     { "int", 0 },
-    { "char", 0 },
+    { "include", 0 }, 
+    { "unsigned", 0 },
     { "while" , 0},
-    { "if", 0}
 };
 
-int binary_search(char *word, struct key  tab[], int n) {
+int binary_search(char *word, struct key tab[], int n) {
     int low = 0;
     int high = n - 1;
     int mid;
