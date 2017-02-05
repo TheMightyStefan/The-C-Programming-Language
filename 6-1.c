05d0f26aed9d09ddefefacbcd64eca194d4cfc0d
TheMigthyStefan <stefan.1.stfanache@gmail.com>
2017-02-05 12:12:09 +0000

    Revised getword(). 6-1

--- 6-1.c
+++ 6-1.c
@@ -4,8 +4,9 @@
 #include <string.h>
 
 #define NKEYS (sizeof(keytab) / sizeof(struct key))
-#define MAXWORDS 100
-#define BUFFER_SIZE 100
+#define MAXWORDS 10000
+#define BUFFER_SIZE 1000000
+#define MAX_LENGTH 1000000
 
 char buffer[100];
 int buffer_index = 0;
@@ -47,11 +48,11 @@ int get_word(char *word, int limit) {
                     unget_character(temp_character);
             } else
                 unget_character(character);
-        } else 
+        } else if (character != '(' && character != '*' && character != '[' && character != '!' && character != '&' && character != '-' && character != '+') 
             for ( ; !isspace(character) && character != EOF; character = get_character());
 
-        if (character != '\"' && character != '\n' && character != '/')
-            unget_character(character);
+        //if (character != '\"' && character != '\n' && character != '/')
+         //   unget_character(character);
     
         *word_ptr = '\0';
         return character; 
@@ -60,7 +61,7 @@ int get_word(char *word, int limit) {
     for ( ; --limit > 0; ) {
         char check_character = get_character();
 
-        if (!isalnum(check_character)) {
+        if (!isalnum(check_character) && check_character != '_') {
             if (!isspace(check_character)) {
                 unget_character(check_character);
                 return check_character;
@@ -75,57 +76,6 @@ int get_word(char *word, int limit) {
     *word_ptr = '\0';
     return word[0];
 }
-
-int main() {
-    char *word_ptr = word;
-    char character;
-    char temp_character;
- 
-    while (isspace(character = get_character()));
-
-    if (character != EOF && isalnum(character))
-        *word_ptr++ = character;
-
-    if (!isalpha(character)) {
-        if (character == '\"')
-            for (character = get_character(); character != '\"'; character = get_character());
-        else if (character == '#')
-            for (character = get_character(); character != '\n'; character = get_character());
-        else if (character == '/') {
-            if ((character = get_character()) == '/')
-                for (character = get_character(); character != '\n'; character = get_character());
-            else if (character == '*') {
-                for (character = get_character(), temp_character = get_character(); character != '*' && temp_character != '/'; character = get_character(), temp_character = get_character())
-                    unget_character(temp_character);
-            } else
-                unget_character(character);
-        } else 
-            for ( ; !isspace(character) && character != EOF; character = get_character());
-
-        if (character != '\"' && character != '\n' && character != '/')
-            unget_character(character);
-    
-        *word_ptr = '\0';
-        return character; 
-    }
-
-    for ( ; --limit > 0; ) {
-        char check_character = get_character();
-
-        if (!isalnum(check_character)) {
-            if (!isspace(check_character)) {
-                unget_character(check_character);
-                return check_character;
-            } else {
-                unget_character(check_character);
-                break;
-            }
-        } else
-            *word_ptr++ = check_character;
-    }
-    
-    *word_ptr = '\0';
-    return word[0];
 
 struct key {
     char *word;
@@ -165,13 +115,29 @@ int binary_search(char *word, struct key tab[], int n) {
     return -1;
 }
 
+void clear_string(char string[MAX_LENGTH]) {
+    for (int index = strlen(string); index > 0; index--)
+        string[index] = 0;
+}
+
 int main() {
     int n;
     char word[MAXWORDS];
 
-    while (get_word(word, MAXWORDS) != EOF)
-        if (isalpha(word[0]))
+    while (get_word(word, MAXWORDS) != EOF) {
+        if (isalpha(word[0])) {
             printf("%s\n", word);
+
+            if ((n = binary_search(word, keytab, NKEYS)) >= 0)
+                keytab[n].count++;
+
+	    clear_string(word);
+        }
+    }
+    
+    for (n = 0; n < NKEYS; n++)
+        if (keytab[n].count > 0)
+            printf("%4d %s\n", keytab[n].count, keytab[n].word);
 
     return 0;
 }
