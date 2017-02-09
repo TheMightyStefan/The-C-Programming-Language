45ab8c5c86d5b3d8bc7f797f7a134af1ca24ef6e
TheMigthyStefan <stefan.1.stfanache@gmail.com>
2017-02-09 16:09:02 +0000

    Minor modifications + splitted get_word(). 6-1

--- 6-1.c
+++ 6-1.c
@@ -3,7 +3,6 @@
 #include <ctype.h>
 #include <string.h>
 
-#define NKEYS (sizeof(keytab) / sizeof(struct key))
 #define MAXWORDS 10000
 #define BUFFER_SIZE 1000000
 #define MAX_LENGTH 1000000
@@ -11,9 +10,9 @@
 char buffer[100];
 int buffer_index = 0;
 
-void unget_character(int character) {
+void unget_character(char character) {
     if (buffer_index >= BUFFER_SIZE)
-        printf("\nUngetch : Too many characters");
+        fprintf(stderr, "Ungetch : Too many characters\n");
     else
         buffer[buffer_index++] = character;
 }
@@ -25,37 +24,56 @@ int get_character() {
         return getchar();
 }
 
+char get_to(char destination) {
+    char character = get_character();
+
+    for ( ; character != destination; character = get_character());
+
+    return character;
+}
+
+char skip(char target) {
+    char character = target;
+
+    if (character == '#')
+        character = get_to('\n');
+    else if (character == '"')
+        character = get_to('"');
+    else if (character == '/') {
+        char temp_character = get_character();
+    
+        if (temp_character == '/')
+            character = get_to('\n');
+        else if (temp_character == '*') {
+            character = get_to('*');
+
+            if (character == '/')
+                character = get_character();
+        } else 
+            unget_character(temp_character);
+    }
+
+    return character;
+}
+
 int get_word(char *word, int limit) {   
     char *word_ptr = word;
-    char character;
-    char temp_character;
+    char character = get_character();
  
-    while (isspace(character = get_character()));
+    while (isspace(character))
+        character = get_character();
 
     if (character != EOF && isalnum(character))
-        *word_ptr++ = character;
+        (*word_ptr++) = character;
 
     if (!isalpha(character)) {
-        if (character == '\"')
-            for (character = get_character(); character != '\"'; character = get_character());
-        else if (character == '#')
-            for (character = get_character(); character != '\n'; character = get_character());
-        else if (character == '/') {
-            if ((character = get_character()) == '/')
-                for (character = get_character(); character != '\n'; character = get_character());
-            else if (character == '*') {
-                for (character = get_character(), temp_character = get_character(); character != '*' && temp_character != '/'; character = get_character(), temp_character = get_character())
-                    unget_character(temp_character);
-            } else
-                unget_character(character);
-        } else if (character != '(' && character != '*' && character != '[' && character != '!' && character != '&' && character != '-' && character != '+') 
-            for ( ; !isspace(character) && character != EOF; character = get_character());
+        character = skip(character);
 
         *word_ptr = '\0';
         return character; 
     }
 
-    for ( ; --limit > 0; ) {
+    while (--limit > 0) {
         char check_character = get_character();
 
         if (!isalnum(check_character) && check_character != '_') {
@@ -64,7 +82,7 @@ int get_word(char *word, int limit) {
                 return check_character;
             } else {
                 unget_character(check_character);
-                break;
+                limit = -1;
             }
         } else
             *word_ptr++ = check_character;
@@ -74,36 +92,57 @@ int get_word(char *word, int limit) {
     return word[0];
 }
 
-struct key {
+struct word_counter {
     char *word;
     int count;
-} keytab[] = {
+}; 
+
+struct word_counter key_words[] = {
     { "auto", 0 },
     { "break", 0},
     { "case", 0 },
     { "char", 0 },
     { "const", 0 },
     { "continue" , 0 },
-    // ........... //
     { "default", 0 },
     { "define", 0 },
+    { "do" , 0 },
+    { "double", 0 },
+    { "else", 0 },
+    { "enum", 0 },
+    { "extern", 0 },
+    { "float", 0 },
+    { "for", 0 },
+    { "goto", 0 },
     { "if", 0 },
     { "int", 0 },
-    { "include", 0 }, 
+    { "long", 0 },
+    { "register", 0 },
+    { "return", 0 },
+    { "short", 0 },
+    { "signed", 0 },
+    { "sizeof", 0 },
+    { "static", 0 },
+    { "struct", 0 },
+    { "switch", 0 },
+    { "typedef", 0 },
+    { "union", 0 },
     { "unsigned", 0 },
-    { "while" , 0},
+    { "void", 0 },
+    { "volatile", 0 },
+    { "while" , 0}
 };
 
-int binary_search(char *word, struct key tab[], int n) {
+int binary_search(char *word, struct word_counter table[], int n) {
     int low = 0;
     int high = n - 1;
-    int mid;
+    int mid = 0;
  
     while (low <= high) {
         mid = (low + high) / 2;
-        if (strcmp(word, tab[mid].word) < 0)
+        if (strcmp(word, table[mid].word) < 0)
             high = mid - 1;
-        else if (strcmp(word, tab[mid].word) > 0)
+        else if (strcmp(word, table[mid].word) > 0)
             low = mid + 1;
         else
             return mid;
@@ -113,9 +152,11 @@ int binary_search(char *word, struct key tab[], int n) {
 }
 
 void clear_string(char string[MAX_LENGTH]) {
-    for (int index = strlen(string); index > 0; index--)
+    for (int index = strlen(string) - 1; index >= 0; index--)
         string[index] = 0;
 }
+
+#define NKEYS (sizeof(key_words) / sizeof(struct word_counter))
 
 int main() {
     int n;
@@ -123,16 +164,16 @@ int main() {
 
     while (get_word(word, MAXWORDS) != EOF) {
         if (isalpha(word[0])) {
-            if ((n = binary_search(word, keytab, NKEYS)) >= 0)
-                keytab[n].count++;
+            if ((n = binary_search(word, key_words, NKEYS)) >= 0)
+                key_words[n].count++;
 
 	    clear_string(word);
         }
     }
-    
+
     for (n = 0; n < NKEYS; n++)
-        if (keytab[n].count > 0)
-            printf("%4d %s\n", keytab[n].count, keytab[n].word);
+        if (key_words[n].count > 0)
+            printf("%4d %s\n", key_words[n].count, key_words[n].word);
 
     return 0;
 }
